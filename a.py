dd02b44671109cd99cf828fb1dccd09999ee6426
occasionallydavid <62972099+occasionallydavid@users.noreply.github.com>
2022-07-05 20:26:23 +0100

    pad it out more

--- a.py
+++ a.py
@@ -1,5 +1,31 @@
 
 import mailpar
 
-print((mailpar.parse_mail(b"x"*100000)))
-print((mailpar.parse_mail(open('/tmp/foo2.eml', 'rb').read())))
+s = open('/tmp/foo.mbox', 'rb').read()
+
+if 1:
+    msg = mailpar.parse_mail(s)
+    print('msg:', msg)
+    print('msg.path():', msg.path())
+    print('msg.raw_byte_offset():', msg.raw_byte_offset())
+    print('msg.mime_type():', msg.mime_type())
+    print('msg.charset():', msg.charset())
+    print('msg.params():', msg.params())
+    print('msg.subpart_count():', msg.subpart_count())
+    print('msg.get_body():', repr(msg.get_body())[:100])
+    print('msg.get_body_raw():', repr(msg.get_body_raw())[:100])
+    print()
+
+    for i in range(msg.subpart_count()):
+        print(f'msg.subpart({i}):')
+        part = msg.subpart(i)
+        print('  mime_type():', part.mime_type())
+        print('  path():', part.path())
+        print('  charset():', part.charset())
+        print('  params():', msg.params())
+        print('  param(boundary):', msg.param("boundary"))
+        print('  subpart_count():', part.subpart_count())
+        print('  get_body():', repr(part.get_body())[:100])
+        print('  get_body_raw():', repr(part.get_body_raw())[:100])
+        print('  get_body_encoded():', repr(part.get_body_encoded())[:100])
+        print()
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
+#[allow(unused_imports)]
 use pyo3::prelude::*;
+#[allow(unused_imports)]
 use pyo3::types::*;
+#[allow(unused_imports)]
 use owning_ref::OwningHandle;
+#[allow(unused_imports)]
+use std::rc::Rc;
+use std::collections::BTreeMap;
+use mailparse::body::Body::Binary;
+use mailparse::body::Body::Base64;
+use mailparse::body::Body::QuotedPrintable;
+use mailparse::body::Body::SevenBit;
+use mailparse::body::Body::EightBit;
 
-use pyo3::exceptions::PyZeroDivisionError;
 use pyo3::exceptions::PyException;
+use pyo3::exceptions::PyIndexError;
 use pyo3::create_exception;
 
 use mailparse;
@@ -13,14 +24,13 @@ use mailparse;
 create_exception!(mailpar, ParseError, PyException);
 
 
-//fn slice_offset(parent: &[u8], child: &[u8]) -> usize {
-    //(child.as_ptr() as usize) - (parent.as_ptr() as usize)
-//}
+fn slice_offset(parent: &[u8], child: &[u8]) -> usize {
+    (child.as_ptr() as usize) - (parent.as_ptr() as usize)
+}
 
 
-#[pyclass]
-struct PyParsedMail {
-    ha: OwningHandle<
+struct MailStorage {
+    handle: OwningHandle<
         Box<Vec<u8>>,
         Box<
             Result<
@@ -32,25 +42,145 @@ struct PyParsedMail {
 }
 
 
-#[pymethods]
-impl PyParsedMail {
-
+#[pyclass(unsendable)]
+struct PyParsedMail {
+    storage: Rc<MailStorage>,
+    path: Vec<usize>
 }
 
-#[pyfunction]
-fn parse_mail<'a>(py: Python<'a>, buf: &[u8]) -> PyResult<PyParsedMail>
-{
-    let pm = PyParsedMail {
-        ha: OwningHandle::new_with_fn(
-            Box::new(buf.to_vec()),
-            unsafe {
-                |x| Box::new(mailparse::parse_mail((*x).as_slice()))
-            }
-        )
-    };
 
-    match &*pm.ha {
-        Ok(_) => Ok(pm),
+#[pyclass(unsendable)]
+struct PyHeaders {
+    storage: Rc<MailStorage>,
+    path: Vec<usize>
+}
+
+
+#[pymethods]
+impl PyHeaders {
+}
+
+
+fn _part<'a>(parsed: &'a PyParsedMail) -> &'a mailparse::ParsedMail<'a>
+{
+    let handle = &(parsed.storage.handle);
+    let result = &*handle;
+
+    let mut part = result.as_ref().unwrap();
+    for i in &parsed.path {
+        //println!("EEK {}", i);
+        part = &(part.subparts[*i]);
+    }
+
+    part
+}
+
+#[pymethods]
+impl PyParsedMail {
+    fn raw_byte_offset(&self) -> usize {
+        let handle = &(self.storage.handle);
+        slice_offset(handle.as_owner().as_slice(), _part(self).raw_bytes)
+    }
+
+    fn subpart_count(&self) -> usize {
+        _part(self).subparts.len()
+    }
+
+    fn mime_type(&self) -> &String {
+        &(_part(self).ctype.mimetype)
+    }
+
+    fn charset(&self) -> &String {
+        &(_part(self).ctype.charset)
+    }
+
+    fn params(&self) -> BTreeMap<String, String> {
+        _part(self).ctype.params.clone()
+    }
+
+    fn param(&self, k: &str) -> Option<&String> {
+        _part(self).ctype.params.get(k)
+    }
+
+    fn path(&self) -> Vec<usize> {
+        self.path.clone()
+    }
+
+    fn headers(&self) -> PyHeaders {
+        PyHeaders {
+            storage: self.storage.clone(),
+            path: self.path.clone()
+        }
+    }
+
+    fn subpart(&self, i: usize) -> PyResult<PyParsedMail> {
+        let part = _part(self);
+        if i >= part.subparts.len() {
+            return Err(PyIndexError::new_err(i));
+        }
+
+        Ok(PyParsedMail {
+            storage: self.storage.clone(),
+            path: _subpath(&self.path, i)
+        })
+    }
+
+    fn get_body(&self) -> PyResult<String> {
+        match _part(self).get_body() {
+            Ok(s) => Ok(s),
+            Err(e) => Err(ParseError::new_err(e.to_string()))
+        }
+    }
+
+    fn get_body_raw(&self, py: Python) -> PyResult<PyObject> {
+        match _part(self).get_body_raw() {
+            Ok(s) => Ok(PyBytes::new(py, s.as_slice()).into()),
+            Err(e) => Err(ParseError::new_err(e.to_string()))
+        }
+    }
+
+    fn get_body_encoded(&self, py: Python) -> PyResult<PyObject> {
+        let s = match _part(self).get_body_encoded() {
+            Base64(eb) => eb.get_raw(),
+            QuotedPrintable(eb) => eb.get_raw(),
+            SevenBit(tb) => tb.get_raw(),
+            EightBit(tb) => tb.get_raw(),
+            Binary(bb) => bb.get_raw(),
+        };
+
+        Ok(PyBytes::new(py, s).into())
+    }
+}
+
+
+fn _subpath(path: &Vec<usize>, i: usize) -> Vec<usize> {
+    let mut new = path.clone();
+    new.push(i);
+    new
+}
+
+
+#[pyfunction]
+fn parse_mail<'a>(_py: Python<'a>, buf: &[u8]) -> PyResult<PyParsedMail>
+{
+    let handle = OwningHandle::new_with_fn(
+        Box::new(buf.to_vec()),
+        unsafe {
+            |x| Box::new(mailparse::parse_mail((*x).as_slice()))
+        }
+    );
+
+    match &*handle {
+        Ok(_) => Ok(
+            PyParsedMail {
+                storage: Rc::new(
+                    MailStorage {
+                        handle: handle,
+                    }
+                ),
+                path: vec![]
+            }
+        ),
         Err(error) => Err(ParseError::new_err(error.to_string()))
     }
 }
@@ -60,6 +190,7 @@ fn parse_mail<'a>(py: Python<'a>, buf: &[u8]) -> PyResult<PyParsedMail>
 fn mailpar(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parse_mail, m)?)?;
     m.add_class::<PyParsedMail>()?;
+    m.add_class::<PyHeaders>()?;
     m.add("ParseError", py.get_type::<ParseError>())?;
     Ok(())
 }
